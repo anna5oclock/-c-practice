6a30e3020a24b860056580b3d5c57c9bd7ac6404
Anna <anna5oclock@mail.ru>
2022-06-13 01:05:30 +0300

    Hometask4

---
+++ Homework4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+using System;
+using static System.Console;
+Console.Clear();
+  Console.Write("Write number 1: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Write number 2: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    
