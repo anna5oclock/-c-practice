dda4b818c35f57caec81753862224361c996bb2c
Anna <anna5oclock@mail.ru>
2022-06-13 01:07:23 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+int CurentN = N * -1;
+while (CurentN <= N)
+{
+    Console.Write(CurentN);
+    Console.Write(", ");
+    CurentN ++;
+}
+Console.WriteLine ("End");
