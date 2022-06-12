3a7a511b7186f84e5499a2a08612689206cb2de0
Anna <anna5oclock@mail.ru>
2022-06-13 01:07:45 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число N: ");
+int N = Convert.ToInt32(ReadLine());
+ if ((Math.Abs (N) < 100) || (Math.Abs (N) > 999)) {    
+       Console.WriteLine("Введите не трехзначное число");
+       return;
+}
+Console.WriteLine(N%10);
+
+Console.WriteLine(N/10);
