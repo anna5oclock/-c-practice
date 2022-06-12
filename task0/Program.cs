f831c6d1a31617985920ec26048bc3e04439105a
Anna <anna5oclock@mail.ru>
2022-06-13 01:06:15 +0300

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Console.Clear();
+Console.WriteLine("Wright number");
+int number = Convert.ToInt32(ReadLine());
+//int number1 = int.Parse(Console.ReadLine());
+
+int sqrNumber = number*number;
+//int sqrNumber1 = Convert.ToInt32(Math.Pow(number1, 2));
+
+Console.WriteLine(sqrNumber);
+
