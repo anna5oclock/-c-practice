4b58637af8b3820549721176916ce0bb1845b47a
Anna <anna5oclock@mail.ru>
2022-06-13 01:06:42 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Console.Clear();
+Console.WriteLine("Введите число a = ");
+int a = Convert.ToInt32(ReadLine());
+Console.WriteLine("Введите число b = ");
+int b = Convert.ToInt32(ReadLine());
+
+if ((a * a) == b) Console.WriteLine("Число b является квадратом числа a");
+else                Console.WriteLine("Число b не является квадратом числа a");
+
+
